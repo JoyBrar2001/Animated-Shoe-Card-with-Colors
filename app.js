e7a13768e903d015a81767c66d219d7995a95b43
Joy Brar <joybrar20001@gmail.com>
2023-04-27 22:21:38 +0530

    Final, added Media Queries

--- app.js
+++ app.js
@@ -20,8 +20,14 @@ const shoes = document.querySelectorAll('.shoe');
 const gradients = document.querySelectorAll('.gradient');
 
 let prevColor = "blue";
+let animationEnd = true;
 
 function changeColor(){
+
+    if(animationEnd === false){
+        console.log("Error");
+        return;
+    }
     // Color Dot
     let primary = this.getAttribute('primary');
 
@@ -41,15 +47,39 @@ function changeColor(){
     // Displaying the Gradient
     let gradient = document.querySelector(`.gradient[color="${color}"]`);
     let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`)
-    
-    console.log(gradient)
-    console.log(prevGradient)
 
     gradients.forEach(g => g.classList.remove('first', 'second'));
     prevGradient.classList.add('second');
     gradient.classList.add('first');
 
     prevColor = color;
+
+    animationEnd = false;
+
+    gradient.addEventListener('animationend', () => {
+        animationEnd = true;
+    })
 }
 
 colors.forEach(color => color.addEventListener('click', changeColor));
+
+
+
+
+let x = window.matchMedia("(max-width: 1000px)");
+
+const shoeBg = document.querySelector('.shoeBackground');
+
+function changeHeight(){
+    if(x.matches){
+        let shoeHeight = shoes[0].offsetHeight;
+        shoeBg.style.height = `${shoeHeight * 0.9}px`;
+    }
+    else{
+        shoeBg.style.height = "475px";
+    }
+}
+
+changeHeight();
+
+window.addEventListener('resize', changeHeight);
